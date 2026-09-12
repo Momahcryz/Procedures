88f2c3b46caa4356433e377e6852616c68199499
Momahcryz <cenatusmaanah@gmail.com>
2026-09-12 19:15:52 -0300

    Atualiza repositorio.sql com a versão exata das procedures

--- repositorio.sql
+++ repositorio.sql
@@ -3,28 +3,40 @@ USE Gerenciador_de_Estoque;
 DELIMITER //
 
 CREATE PROCEDURE sp_remocao_produtos(IN p_id INT)
+
 BEGIN 
    DELETE FROM Produtos
    WHERE ID = p_id;
+   
 END //
 
+
 CREATE PROCEDURE sp_remocao_Estoque(IN p_id INT)
+
 BEGIN 
    DELETE FROM Estoque
    WHERE ID = p_id;
+   
 END //
 
+
 CREATE PROCEDURE sp_remocao_Fornecedores(IN p_id INT)
+
 BEGIN 
    DELETE FROM Fornecedores
    WHERE ID = p_id;
+   
 END //
 
+
 CREATE PROCEDURE sp_remocao_Pessoa_Fisica(IN p_id INT)
+
 BEGIN 
    DELETE FROM Pessoa_Fisica
    WHERE ID = p_id;
+   
 END //
+
 
 CREATE PROCEDURE sp_remocao_Pessoa_Juridica(IN p_id INT)
 BEGIN 
@@ -32,11 +44,13 @@ BEGIN
    WHERE ID = p_id;
 END //
 
+
 CREATE PROCEDURE sp_remocao_Consumidores(IN p_id INT)
 BEGIN 
    DELETE FROM Consumidores
    WHERE ID = p_id;
 END //
+
 
 CREATE PROCEDURE sp_remocao_Vendas(IN p_id INT)
 BEGIN 
@@ -45,3 +59,6 @@ BEGIN
 END //
 
 DELIMITER ;
+
+
+DELIMITER //
